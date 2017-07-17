49cf16aa7f65bc7b79b1b4f467989d75b7f5e929
jimgrea@taou.com <!@#606187jim>
2017-07-17 15:21:47 +0800

    add

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(
+    name='sanic_mysql',
+    version='0.0.1',
+    description='Adds mysql support to sanic .',
+    long_description='sanic_mysql is a sanic framework extension which adds support for the mysql.',
+    url='https://github.com/jimgreat/sanic-redis.git',
+    author='jimgreat',
+    license='MIT',
+    packages=['sanic_mysql'],
+    install_requires=('sanic', 'aiomysql'),
+    zip_safe=False,
+    keywords=['sanic', 'mysql', 'aiomysql'],
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Internet :: WWW/HTTP :: Session',
+    ]
+)
