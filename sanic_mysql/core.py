fccbe17d2dd0d2ef3818cc6cce6181b11e239e1b
Kaio Henrique <kaiohfernandes15@gmail.com>
2017-12-27 09:22:34 -0200

    Log support to Sanic 0.5.4
    The importing "sanic.log" doesn't work in actual Sanic version, then is necessary used the "import logging"

--- sanic_mysql/core.py
+++ sanic_mysql/core.py
@@ -1,15 +1,26 @@
-from sanic.log import log
 from aiomysql import create_pool
 import os
-
+import logging
 
 class SanicMysql:
     def __init__(self, app, mysql_config=None):
         self.app = app
         self.config = mysql_config
+        
+        logging_format = "[%(asctime)s] %(process)d-%(levelname)s "
+        logging_format += "%(module)s::%(funcName)s():l%(lineno)d: "
+        logging_format += "%(message)s"
+
+        logging.basicConfig(
+            format=logging_format,
+            level=logging.DEBUG
+        )
+
+        self.log = logging.getLogger()
 
         if app:
             self.init_app(app=app)
+    
 
     async def start(self, _app, loop):
         _k = dict(loop=loop)
@@ -21,13 +32,13 @@ class SanicMysql:
         _k.update(config)
 
         _mysql = await create_pool(**_k)
-        log.info('opening mysql connection for [pid:{}]'.format(os.getpid()))
+        self.log.info('opening mysql connection for [pid:{}]'.format(os.getpid()))
 
         async def _query(sqlstr, args=None):
             async with _mysql.acquire() as conn:
                 async with conn.cursor() as cur:
                     final_str = cur.mogrify(sqlstr, args)
-                    log.info('mysql query [{}]'.format(final_str))
+                    self.log.info('mysql query [{}]'.format(final_str))
                     await cur.execute(final_str)
                     value = await cur.fetchall()
                     return value
@@ -45,5 +56,5 @@ class SanicMysql:
         @app.listener('after_server_stop')
         async def close_mysql(_app, loop):
             _app.mysql.close()
-            log.info('closing mysql connection for [pid:{}]'.format(os.getpid()))
+            self.log.info('closing mysql connection for [pid:{}]'.format(os.getpid()))
             await _app.mysql.wait_closed()
